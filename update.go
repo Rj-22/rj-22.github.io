f89419d11562b01a003a0910196c60d725ebff39
Milad Karami <miladkarami990830@gmail.com>
2022-02-05 18:11:12 +0330

    update website

--- update.go
+++ update.go
@@ -19,9 +19,6 @@ func main() {
 	}
 	fmt.Println("Initializing git...")
 	out, err := exec.Command("git", "init").Output()
-	fmt.Println("adding new branch...")
-	err = exec.Command("git", "remote", "add", "https://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@github.com/rj-22/rj-22.github.io.git").Run()
-	fmt.Println(err.Error())
 	fmt.Println("adding new files...")
 	out, err = exec.Command("git", "add", ".").Output()
 	fmt.Println("commiting current working directory...")
