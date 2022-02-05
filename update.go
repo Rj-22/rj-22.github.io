e684d9a89d823147c1f1cd56b25769b3d0154702
Milad Karami <miladkarami990830@gmail.com>
2022-02-05 17:59:54 +0330

    update website

--- update.go
+++ update.go
@@ -7,20 +7,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Please install git before upgrading.(https://git-scm.com/downloads)")
 	var git_installed string = " "
 	for git_installed != "y" && git_installed != "n" {
 		fmt.Print("Did you installed git already?(y/n) ")
 		fmt.Scan(&git_installed)
 	}
 	if git_installed == "n" {
+		fmt.Println("Please install git before upgrading.(https://git-scm.com/downloads)")
+		fmt.Scan(&git_installed)
 		os.Exit(0)
 	}
+	fmt.Println("Initializing git...")
 	out, err := exec.Command("git", "init").Output()
+	fmt.Println("adding new files...")
 	out, err = exec.Command("git", "add", ".").Output()
+	fmt.Println("commiting current working directory...")
 	out, err = exec.Command("git", "commit", "-m", "update website").Output()
+	fmt.Println("pushing to github...")
 	out, err = exec.Command("git", "push", "-u", "https://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@github.com/rj-22/rj-22.github.io.git").Output()
-	out, err = exec.Command("git", "pull", "https://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@github.com/rj-22/rj-22.github.io.git").Output()
-	fmt.Println(out)
+	fmt.Println(string(out))
 	fmt.Println(err)
 }
