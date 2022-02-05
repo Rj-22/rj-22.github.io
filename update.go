91dc4d8b01c41664a92d9198ccdb1762509e3a84
Milad Karami <miladkarami990830@gmail.com>
2022-02-05 19:17:28 +0330

    "update"

--- update.go
+++ update.go
@@ -24,21 +24,10 @@ func main() {
 	fmt.Println("commiting current working directory...")
 	exec.Command("git", "commit", "-m", "\"update\"").Run()
 
-	fmt.Println(
-		"The account: it.rojdax.com@gmail.com\n"+
-		"Please check the repo checkbox.\n"+
-		"Please create a token here: https://github.com/settings/tokens")
-
-	vari = " "
-	for vari == " " {
-		fmt.Print("Please enter the personal access token? ")
-		fmt.Scan(&vari)
-	}
-
 	fmt.Println("pushing to github...")
 	exec.Command("git", "push",
-		"https://"+vari+"@github.com/Rj-22/rj-22.github.io.git").Run()
-	
+		"https://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@github.com/Rj-22/rj-22.github.io.git").Run()
+
 	fmt.Println("\nDone")
 	fmt.Scan(&vari)
 }
