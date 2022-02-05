3d11281f53a3bb4c35559eb0507055db0451fba1
Milad Karami <miladkarami990830@gmail.com>
2022-02-05 16:36:30 +0330

    update website

--- update.go
+++ update.go
@@ -12,7 +12,7 @@ func main() {
 	out, err = exec.Command("git", "commit", "-m", "update website").Output()
 	fmt.Println(out)
 	fmt.Println(err)
-	out, err = exec.Command("git", "push", "https://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@github.com/rj-22/rj-22.github.io.git").Output()
+	out, err = exec.Command("git", "push", "-u", "https://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@github.com/rj-22/rj-22.github.io.git").Output()
 	fmt.Println(out)
 	fmt.Println(err)
 }
