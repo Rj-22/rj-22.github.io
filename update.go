2e7375fe181a90864e0b9ec9fc7bfac5740002ca
Milad Karami <miladkarami990830@gmail.com>
2022-02-05 16:49:39 +0330

    update website

--- update.go
+++ update.go
@@ -8,8 +8,9 @@ import (
 func main() {
 	fmt.Println("Please install git before upgrading.(https://git-scm.com/downloads)")
 	var git_installed byte
-	fmt.Scanf("%c", &git_installed)
-	out, err := exec.Command("git", "add", ".").Output()
+	fmt.Sscanln("Did you installed git already?(y/n) %c", &git_installed)
+	out, err := exec.Command("git", "init").Output()
+	out, err = exec.Command("git", "add", ".").Output()
 	out, err = exec.Command("git", "commit", "-m", "update website").Output()
 	out, err = exec.Command("git", "push", "-u", "https://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@github.com/rj-22/rj-22.github.io.git").Output()
 	out, err = exec.Command("git", "pull", "https://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@github.com/rj-22/rj-22.github.io.git").Output()
